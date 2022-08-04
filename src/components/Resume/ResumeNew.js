f8918db3b9e448004daea50aef3d49511d4635f0
sourabh7047 <sourabhasharma15@gmail.com>
2022-08-04 18:51:55 +0530

    resume link bug fixed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/sourabh7047/portfolio/master/src/Assets/Sourabh_Asharma_resume.pdf";
+  "https://raw.githubusercontent.com/sourabh7047/Digital_Portfolio/master/src/Assets/Sourabh_Asharma_resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
